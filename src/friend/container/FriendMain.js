ecb780f1fad282a46ed00bb69d86f6d1a8ee0488
naseokju <lukas95@naver.com>
2022-11-13 17:03:01 +0900

    redux

--- src/friend/container/FriendMain.js
+++ src/friend/container/FriendMain.js
@@ -1,28 +1,18 @@
-import React, { useEffect, useReducer } from "react";
+import React from "react";
 import store from "../../common/store";
 import { getNextFriend } from "../../common/mockData";
 import { addFriend } from "../state";
 import FriendList from "../component/FriendList";
+import { useSelector, useDispatch } from "react-redux";
 
 export default function FriendMain() {
-    const [, forceUpdate] = useReducer(v => v + 1, 0);
-    useEffect(() => {
-        let prevFriends = store.getState().friend.friends;
-        const unsubscribe = store.subscribe(() => {
-            const friends = store.getState().friend.friends;
-            if (prevFriends !== friends) {
-                forceUpdate();
-            }
-            prevFriends = friends;
-        });
-        return () => unsubscribe();
-    }, []);
+    const friends = useSelector(state => state.friend.friends);
+    const dispatch = useDispatch();
     function onADD() {
         const friend = getNextFriend();
         store.dispatch(addFriend(friend));
     }
     console.log("FriendMain render");
-    const friends = store.getState().friend.friends;
     return (
         <div>
             <button onClick={onADD}>친구 추가</button>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import store from "./common/store";
 import FriendMain from "./friend/container/FriendMain";
 import TimelineMain from "./timeline/container/TimelineMain";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
-  <div>
-    <FriendMain />
-    <TimelineMain />
-  </div>,
+  <Provider store={store}>
+    <div>
+      <FriendMain />
+      <TimelineMain />
+    </div>
+  </Provider>,
   document.getElementById("root")
 );
